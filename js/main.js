0a8e8e151d2e4370751ea2a684e84dcb79c4fb4c
Chugh <a_chugh70782@tss-hr420-dm26.fc.ca>
2018-06-13 14:02:46 -0400

    js updated

--- js/main.js
+++ js/main.js
@@ -3,13 +3,24 @@
 	console. log("js file loaded") ;
 
 	//vars
-	
+	var num1 = document.querySelector("#input1");
+	var num2 = document.querySelector("#input2");
+	var equals = document.queryselector("#trigger");
+	var outcome = document.queryselector("#value");
+	//console.log(num1);
+	//console.log(num2);
+	//console.log(equals);
+	//console.log(outcome);
+
 	
 
 
-	
+
 	//functions
-	
+	function addStuff(){
+		//console. log("from addStuff");
+
+	}
 
 
 
@@ -17,5 +28,6 @@
 
 
 	//listner
+	equals.addEvenListner("click", addStuff, false);
 
 }) ();
